60ff0b68610ecb8a2f5446c64e4273c2b569c404
Sradem <ademmgur@gmail.com>
2015-10-07 18:07:08 +0200

    Instruction class, ok.

---
+++ StackMachine/src/fr/iutvalence/info/m3105/stackmachine/Instruction.java
@@ -0,0 +1,15 @@
+package fr.iutvalence.info.m3105.stackmachine;
+
+public class Instruction
+{
+	private int opCode;
+	private int params;
+
+	public int getOpCode(){
+		return this.opCode;
+	}
+	
+	public int getParams(){
+		return this.params;
+	}
+}
