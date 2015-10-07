30c932e8d11fea020b7b986834f89e89ee4c6053
Sradem <ademmgur@gmail.com>
2015-10-07 19:16:47 +0200

    Creat class Stack

---
+++ StackMachine/src/fr/iutvalence/info/m3105/stackmachine/Stack.java
@@ -0,0 +1,14 @@
+package fr.iutvalence.info.m3105.stackmachine;
+
+public class Stack
+{
+	private int[] tableau;
+	
+	public Stack(int taille){
+		this.tableau = new int[taille];
+	}
+	
+	public void clearStack(){
+	
+	}
+}
