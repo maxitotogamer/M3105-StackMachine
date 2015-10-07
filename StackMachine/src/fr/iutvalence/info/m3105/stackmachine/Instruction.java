9b0cd51b6e111f0086a846ca67e2722bbbd29316
Sradem <ademmgur@gmail.com>
2015-10-07 18:15:24 +0200

    Fix class Instruction.

--- StackMachine/src/fr/iutvalence/info/m3105/stackmachine/Instruction.java
+++ StackMachine/src/fr/iutvalence/info/m3105/stackmachine/Instruction.java
@@ -3,13 +3,18 @@ package fr.iutvalence.info.m3105.stackmachine;
 public class Instruction
 {
 	private int opCode;
-	private int params;
+	private int[] params;
+	
+	public Instruction(int opCode, int[] params){
+		this.opCode = opCode;
+		this.params = params;
+	}
 
 	public int getOpCode(){
 		return this.opCode;
 	}
 	
-	public int getParams(){
+	public int[] getParams(){
 		return this.params;
 	}
 }
